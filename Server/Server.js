06d5f5a7d0fb15b0628a7c441d9fe84f8fb621c6
Raga-dharshini04 <sragadharshini0@gmail.com>
2026-03-08 15:34:49 +0530

    Finalized route order for deployment

--- Server/Server.js
+++ Server/Server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const studentRoutes = require("./routes/studentRoutes");
-
 
 dotenv.config();
 connectDB();
@@ -13,19 +13,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Backend running successfully");
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log("Mongo URI:", process.env.MONGO_URI);
-});
 
 const adminRoutes = require("./routes/adminRoutes");
 app.use("/api/admin", adminRoutes);
@@ -42,3 +29,20 @@ app.use("/api/courses", courseRoutes);
 
 app.use("/uploads", express.static("uploads"));
 app.use("/api/student", studentRoutes);
+
+app.use("/api/auth", authRoutes);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log("Mongo URI:", process.env.MONGO_URI);
+});
+
